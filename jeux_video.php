6c9dc5502886a7e65ac1780113a52a2c02b196f6
Lucile Rosellini <lucile.rosellini@Digital-Grenoble.local>
2020-02-18 14:43:03 +0100

    v6

--- jeux_video.php
+++ jeux_video.php
@@ -40,7 +40,7 @@ $reponse = $bdd->query('SELECT console, nom FROM jeux_video WHERE console= "NES"
 while ($donnees = $reponse->fetch())
 {
 echo '<p>'. $donnees['console'] . ' - '. $donnees['nom'] . '</p>';
-}*/
+}
 
 //On peut filtrer avec WHERE pour demander uniquement les noms des jeux sur la console "NES" ou "PC" /!/ On remarquera qu'il est possible d'utiliser le "OR" de PHP pour un même resultat. 
 $bdd = new PDO('mysql:host=localhost;dbname=jeuvideo','root','', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
@@ -48,8 +48,15 @@ $reponse = $bdd->query('SELECT console, nom FROM jeux_video WHERE console= "NES"
 while ($donnees = $reponse->fetch())
 {
 echo '<p>'. $donnees['console'] . ' - '. $donnees['nom'] . '</p>';
-}
+}*/
 
+//On peut filtrer avec WHERE pour demander uniquement les noms des jeux sur la console "NES" ou "PC" et trier grâce à ORDER BY.
+$bdd = new PDO('mysql:host=localhost;dbname=jeuvideo','root','', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+$reponse = $bdd->query('SELECT console, nom FROM jeux_video WHERE console= "NES" OR console= "PC" ORDER BY nom');
+while ($donnees = $reponse->fetch())
+{
+echo '<p>'. $donnees['console'] . ' - '. $donnees['nom'] . '</p>';
+}
 
 
 ?>
