f437c60a5e05157032c529cc93211deb39cd2727
Lucile Rosellini <lucile.rosellini@Digital-Grenoble.local>
2020-02-18 14:18:41 +0100

    V2

--- jeux_video.php
+++ jeux_video.php
@@ -6,7 +6,7 @@ $reponse = $bdd->query('SELECT*FROM jeux_video');
 while ($donnees = $reponse->fetch())
 {
 echo '<p>'. $donnees['nom'].'</p>';
-}*/
+}
 
 
 //Par exemple: Demander le nom des jeux video + la console
@@ -15,7 +15,17 @@ $reponse = $bdd->query('SELECT*FROM jeux_video');
 while ($donnees = $reponse->fetch())
 {
 echo '<p>'. $donnees['console'] . ' - '. $donnees['nom'] . '</p>';
+}*/
+
+
+//Gestion des erreurs lié à Fetch. erreur ici matérialisé dans jeux_video à qui on a supprimé le "é" ou bien mal ecrit "FROM"...
+$bdd = new PDO('mysql:host=localhost;dbname=jeuvideo','root','', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+$reponse = $bdd->query('SELECT*FROM jeux_vido');
+while ($donnees = $reponse->fetch())
+{
+echo '<p>'. $donnees['console'] . ' - '. $donnees['nom'] . '</p>';
 }
+
 
 
 ?>
