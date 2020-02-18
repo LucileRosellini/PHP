1753fc32cada03a0a846c21234e4c3d3e4e7e4e0
Lucile Rosellini <lucile.rosellini@Digital-Grenoble.local>
2020-02-18 14:34:57 +0100

    v4

--- jeux_video.php
+++ jeux_video.php
@@ -24,7 +24,7 @@ $reponse = $bdd->query('SELECT*FROM jeux_vido');
 while ($donnees = $reponse->fetch())
 {
 echo '<p>'. $donnees['console'] . ' - '. $donnees['nom'] . '</p>';
-}*/
+}
 
 //Demander SEULEMENT le nom des jeux video + la console
 $bdd = new PDO('mysql:host=localhost;dbname=jeuvideo','root','', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
@@ -32,7 +32,14 @@ $reponse = $bdd->query('SELECT console, nom FROM jeux_video');
 while ($donnees = $reponse->fetch())
 {
 echo '<p>'. $donnees['console'] . ' - '. $donnees['nom'] . '</p>';
-}
+}*/
 
+//On peut filtrer avec WHERE pour demander uniquement les noms des jeux sur la console "NES"
+$bdd = new PDO('mysql:host=localhost;dbname=jeuvideo','root','', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+$reponse = $bdd->query('SELECT console, nom FROM jeux_video WHERE console= "NES"');
+while ($donnees = $reponse->fetch())
+{
+echo '<p>'. $donnees['console'] . ' - '. $donnees['nom'] . '</p>';
+}
 ?>
 
